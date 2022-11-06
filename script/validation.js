7311234c27008321d14424d83211f7e1422512a6
CantarinoG <guilhermecantarino1709@gmail.com>
2022-11-06 19:01:57 -0300

    Added a check for matching passwords

--- script/validation.js
+++ script/validation.js
@@ -1,3 +1,24 @@
 document.querySelector("#submit").addEventListener("click", function() {
     document.getElementById("form").className = "submitted";
+
 });
+
+let pass1 = document.querySelector('#pass1');
+let pass2 = document.querySelector('#pass2');
+let label = document.querySelector('label[for="pass2"');
+
+pass1.addEventListener('input', () => {
+    if (pass1.value != pass2.value) {
+        label.classList.add('tooltip-js');
+    } else {
+        label.classList.remove('tooltip-js');
+    }
+})
+
+pass2.addEventListener('input', () => {
+    if (pass1.value != pass2.value) {
+        label.classList.add('tooltip-js');
+    } else {
+        label.classList.remove('tooltip-js');
+    }
+})
